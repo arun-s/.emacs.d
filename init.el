fc6c9861fccd54c922e34d4134bdb39ccbc1b55f
Arun Sivakumaran <aruns@outlook.com>
2017-03-13 23:17:22 -0700

    Initial Commit

---
+++ init.el
@@ -0,0 +1,29 @@
+(require 'package)
+(setq package-enable-at-startup nil)
+
+;;; add external repositories
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/"))
+
+(package-initialize)
+
+;;bootstrap use-package
+(unless (package-installed-p 'use-package)
+        (package-refresh-contents)
+	(package-install 'use-package))
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (multiple-cursors use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
